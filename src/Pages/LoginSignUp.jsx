f084751f3853d6f58b18ecb37527e43694527b71
Abhinav Bajpai <74667440+abhinavbajpayee@users.noreply.github.com>
2025-07-10 11:15:00 +0530

    done till login button

--- src/Pages/LoginSignUp.jsx
+++ src/Pages/LoginSignUp.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
-
+import './CSS/LoginSignUp.css'
 export const LoginSignUp = () => {
   return (
-    <div>LoginSignUp</div>
+    <div className='loginsignup'>
+      <div className="loginsignup-container">
+        <h1>Sign UP</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder='Your Name'/>
+          <input type="text" placeholder='Email Address' />
+          <input type="text" placeholder='Password' />
+        </div>
+        <button>Continue</button>
+        <p className="logindignup-login">
+          Already have an account? <span>Login here</span>
+        </p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name='' id=''/>
+          <p>By continuing i agree to the terms of use & privacy policy  </p>
+        </div>
+      </div>
+    </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -8,10 +8,12 @@ import ShopContextProvider from './Context/ShopContext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-    <ShopContextProvider/>
+    <ShopContextProvider>
+      <App />
+    </ShopContextProvider>
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
